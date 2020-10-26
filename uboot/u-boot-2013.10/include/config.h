331832d48c448013520e0092f4eb5ad863961adc
conjimmy <13174605@qq.com>
2020-10-26 09:06:29 -0700

    delte uboot-1.3.4

---
+++ uboot/u-boot-2013.10/include/config.h
@@ -0,0 +1,13 @@
+/* Automatically generated - do not edit */
+#define CONFIG_SYS_ARCH  "arm"
+#define CONFIG_SYS_CPU   "armv7"
+#define CONFIG_SYS_BOARD "goni"
+#define CONFIG_SYS_VENDOR "samsung"
+#define CONFIG_SYS_SOC    "s5pc1xx"
+#define CONFIG_BOARDDIR board/samsung/goni
+#include <config_cmd_defaults.h>
+#include <config_defaults.h>
+#include <configs/s5p_goni.h>
+#include <asm/config.h>
+#include <config_fallbacks.h>
+#include <config_uncmd_spl.h>
---
+++ uboot/u-boot-2013.10/include/config.mk
@@ -0,0 +1,5 @@
+ARCH   = arm
+CPU    = armv7
+BOARD  = goni
+VENDOR = samsung
+SOC    = s5pc1xx
